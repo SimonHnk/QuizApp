4436323aa07fb13ef6e1c1a832f6786000f422bc
Simon Henke <henke.simon@yahoo.de>
2023-12-16 23:45:38 +0100

    designed quizcard style

--- script.js
+++ script.js
@@ -6,5 +6,21 @@ let questions = [
         "answer_3": "Justin Bieber",
         "answer_4": "Tim Berners-Lee",
         "right_answer": 4
-}
+    },
+    {
+        "question": "Was heißt HTML ausgesprochen?",
+        "answer_1": "Horse Toe",
+        "answer_2": "HotMail",
+        "answer_3": "HyperText Markup Language",
+        "answer_4": "How to Make Lasagna",
+        "right_answer": 3
+    },
+    {
+        "question": "Was macht der Tag <br>?",
+        "answer_1": "Text kursiv",
+        "answer_2": "Die Zeile brechen",
+        "answer_3": "Ein Input Feld",
+        "answer_4": "Text Fett",
+        "right_answer": 2
+    }
 ];
