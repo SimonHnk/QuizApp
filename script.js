f8feb22ba6b39edbd122bdecc9ba51c04cb94655
Simon Henke <henke.simon@yahoo.de>
2023-12-16 23:07:28 +0100

    added quizcard

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Justin Bieber",
+        "answer_4": "Tim Berners-Lee",
+        "right_answer": 4
+}
+];
